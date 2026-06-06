55a530542094e1eccd75b9d09893bd73fbc1764b
Amit Kumar Yadav <ameykumar76@gmail.com>
2026-06-06 10:57:46 +0530

    guy

--- app/page.jsx
+++ app/page.jsx
@@ -172,10 +172,6 @@ const PAGE_TREE = [
           { label: 'Endometrial and Ovarian', value: '/reproductive-health-conditions/endometrial-and-ovarian' },
           { label: 'Endometriosis', value: '/reproductive-health-conditions/endometriosis' },
           {
-            label: 'Silent Endometriosis',
-            value: '/reproductive-health-conditions/silent-endometriosis-unexplained-infertility',
-          },
-          {
             label: 'Fibroids, Polyps and Adenomyosis',
             value: '/reproductive-health-conditions/fibroids-polyps-and-adenomyosis',
           },
